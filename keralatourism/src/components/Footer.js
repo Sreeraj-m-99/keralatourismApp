7232739494a7abba9f429c376cd0bcc7a8255a9e
unknown <sreerajmadivayal@gmail.com>
2023-02-18 10:37:28 +0530

    initial commit

---
+++ keralatourism/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h1>footer works</h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ keralatourism/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Header() {
+  return (
+<div className='navBar'>
+        <div className='logo'>Logo</div>
+        <ul className='list'>
+            <li>
+                <a href="">Home</a>
+                </li>
+            <li>
+                <a href="">About</a>
+                </li>
+            <li>
+                <a href="">Contact</a>
+                </li>
+
+
+        </ul>
+
+</div>    )
+}
+
+export default Header
